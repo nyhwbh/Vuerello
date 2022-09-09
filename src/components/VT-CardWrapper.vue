e2c4512e1f5e783e9883d665d80abf097d4e00b9
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2022-09-09 09:30:22 +0900

    add basic element

---
+++ src/components/VT-CardWrapper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card-wrapper">
+    <div class="card-contents">
+      <div class="card-title">
+        <h2 class="card-content-title">To do</h2>
+      </div>
+      <div class="card-lists">
+        <a class="card-content-list">Vue-Go app생성</a>
+        <a class="card-content-list">블록체인 공부하기</a>
+        <a class="card-content-list">프로젝트 계획서 쓰기</a>
+      </div>
+      <div class="add-a-card-controller">
+        <div class="add-a-card-controller-a">
+          <button class="add-card">+ Add a card</button>
+        </div>
+        <div class="add-a-card-controller-b">
+          <input type="text" placeholder="Enter a title this card...">
+          <div>
+            <input type="button" value="Add card">
+            <!-- X 아이콘 -->
+            <!-- ... 아이콘 -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "VTCardWrapper",
+    props: {
+    },
+}
+</script>
+
+<style>
+ .card-contents{
+  display: flex;
+  flex-direction: column;
+ }
+
+ .card-lists{
+  display: flex;
+  flex-direction: column;
+ }
+</style>
