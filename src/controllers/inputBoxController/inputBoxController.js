c58f0fa0e840dbe5aadf2f2231c000a7f107b8f1
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2022-09-11 17:09:46 +0900

    cleaning code(inputboxcontrol)

---
+++ src/controllers/inputBoxController/inputBoxController.js
@@ -0,0 +1,53 @@
+// Add a card, Add another list 클릭시 input box 생성 및 다른 input box 종료
+
+const AddButtons = document.getElementsByClassName('add-a-card-controller-a')
+const closeButtons = document.getElementsByClassName('close-add-card')
+
+// input 박스 켜기
+export const openInputBox = () => {
+    for(var i=0; i<AddButtons.length; i++ ){
+        AddButtons.item(i).addEventListener('click',(events) => {
+            var controllerA = events.path[0].childNodes[0]
+            var controllerB = events.path[0].childNodes[0]
+            if (events.path.length === 10){
+                controllerA = events.path[1].childNodes[0]
+                controllerB = events.path[1].childNodes[1]
+            } else {
+                controllerA = events.path[2].childNodes[0]
+                controllerB = events.path[2].childNodes[1]
+            }
+            inputBoxOpen(controllerA,controllerB)
+        })
+    }
+}
+
+// 켜진 input box 종료하기
+export const closeInputBox = () => {
+    for(var icb=0; icb<closeButtons.length; icb++){   
+        closeButtons[icb].addEventListener('click', () => { 
+        const addAppers=document.getElementsByClassName('add-a-card-controller-a-hidden')
+        const InputDisapper = document.getElementsByClassName('add-a-card-controller-b')
+        if(addAppers.length !== 0){
+            addAppers[0].className='add-a-card-controller-a'
+            InputDisapper[0].className='add-a-card-controller-b-hidden'
+        }
+        })
+    }
+}
+    
+var inputBoxOpen = function(AddDisapper,InputAppers){
+    //켜져있는 다른 inputbox 끄기
+    const addAppers=document.getElementsByClassName('add-a-card-controller-a-hidden')
+    const InputDisapper = document.getElementsByClassName('add-a-card-controller-b')
+    if(addAppers.length !== 0){
+        addAppers[0].className='add-a-card-controller-a'
+        InputDisapper[0].className='add-a-card-controller-b-hidden'
+        //클릭한 inputbox 열기            
+        AddDisapper.className='add-a-card-controller-a-hidden'
+        InputAppers.className='add-a-card-controller-b'
+    }else{
+    // Add a card click 입력받기
+    AddDisapper.className='add-a-card-controller-a-hidden'
+    InputAppers.className='add-a-card-controller-b'
+    }
+}
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import '@fortawesome/fontawesome-free/js/all.js'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import {openInputBox, closeInputBox} from './controllers/inputBoxController/inputBoxController'
+
 createApp(App).mount('#app')
 
 
@@ -30,44 +32,6 @@ for(var il=0; il<listDrags.length; il++){
     });
 }
 
-// add a card button events
-const AddButtons = document.getElementsByClassName('add-a-card-controller-a')
-
-for(var ib=0; ib<AddButtons.length; ib++ ){
-    console.log("for loop "+ ib)
-    console.log(AddButtons.item(ib))
-    AddButtons.item(ib).addEventListener('click',(events) => {
-        console.log(events)
-        var controllerA = events.path[0].childNodes[0]
-        var controllerB = events.path[0].childNodes[0]
-        if (events.path.length === 10){
-            controllerA = events.path[1].childNodes[0]
-            controllerB = events.path[1].childNodes[1]
-        } else {
-            controllerA = events.path[2].childNodes[0]
-            controllerB = events.path[2].childNodes[1]
-        }
-        inputBoxOpen(controllerA,controllerB)
-    })
-}
-
-
-var inputBoxOpen = function(AddDisapper,InputAppers){
-    //켜져있는 다른 inputbox 끄기
-    const addAppers=document.getElementsByClassName('add-a-card-controller-a-hidden')
-    const InputDisapper = document.getElementsByClassName('add-a-card-controller-b')
-    if(addAppers.length !== 0){
-        addAppers[0].className='add-a-card-controller-a'
-        InputDisapper[0].className='add-a-card-controller-b-hidden'
-        //클릭한 inputbox 열기            
-        AddDisapper.className='add-a-card-controller-a-hidden'
-        InputAppers.className='add-a-card-controller-b'
-    }else{
-    // Add a card click 입력받기
-    AddDisapper.className='add-a-card-controller-a-hidden'
-    InputAppers.className='add-a-card-controller-b'
-    }
-}
 
 // add card, add list button events
 const AddCardButtons = document.getElementsByClassName('add-card')
@@ -80,16 +44,6 @@ for(var iclb=0; iclb<AddCardButtons.length; iclb++){
     });
 }
 
-// quit add card button events
-const closeButtons = document.getElementsByClassName('close-add-card')
-for(var icb=0; icb<closeButtons.length; icb++){
-
-    closeButtons[icb].addEventListener('click', () => {
-        const addAppers=document.getElementsByClassName('add-a-card-controller-a-hidden')
-        const InputDisapper = document.getElementsByClassName('add-a-card-controller-b')
-        if(addAppers.length !== 0){
-            addAppers[0].className='add-a-card-controller-a'
-            InputDisapper[0].className='add-a-card-controller-b-hidden'
-        }
-    })
-}
+// inputBoxController
+openInputBox()
+closeInputBox()
