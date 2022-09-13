d2febac6518a1c0402618acb53bbf936e453311b
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2022-09-13 10:55:07 +0900

    add title chang function

--- front/src/controllers/titleController/titleChange.js
+++ front/src/controllers/titleController/titleChange.js
@@ -5,11 +5,16 @@ const textareaValue = document.getElementsByClassName('list-name-change')
 export const switchTitleInput = () => {
     for(var i=0; i<titleClick.length; i++){
         titleClick[i].addEventListener('click', (events) => {
-            console.log("clicked")
             console.log(events)
-            var controllerA = events.path[1]
+            var controllerA = events.path[2].childNodes[0]
             var controllerB = events.path[2].childNodes[1]
-
+            if (events.path.length === 10) {
+                controllerA = events.path[0].childNodes[0]
+                controllerB = events.path[0].childNodes[1]
+            }else if (events.path.length === 11) {
+                controllerA = events.path[1].childNodes[0]
+                controllerB = events.path[1].childNodes[1]
+            }
             titleControl(controllerA,controllerB)
         })
     }
@@ -18,11 +23,18 @@ export const switchTitleInput = () => {
 //textarea 입력받기
 export const changeTitle = () => {
     for(var i=0; i<textareaValue.length; i++){
-        console.log(textareaValue[1])
-        textareaValue[1].addEventListener('keyup', (e) => {
+        textareaValue[i].addEventListener('keydown', (e) => {
             if(e.keyCode === 13){
-                console.log("Enter pressed")
+                e.preventDefault()
                 console.log(e)
+                var changeTitle = e.path[2].childNodes[0].childNodes[0]
+                var changeTextarea = e.path[0]
+                console.log(changeTitle)
+                console.log(changeTitle.innerText)
+                var changeValue = e.target.value
+                console.log(e.target._value)
+                titleClose()
+                titleChange(changeTitle, changeTextarea, changeValue)
             }
         })
     }
@@ -30,6 +42,28 @@ export const changeTitle = () => {
 
 // h3 -> disapper , textarea -> apper
 var titleControl = function(disapper, apper){
-    disapper.className='card-title-controller-a-hide'
-    apper.className='card-title-controller-b'
+    //열려있는 input box 끄기
+    const ctrlA = document.getElementsByClassName('card-title-controller-a-hide')
+    if(ctrlA.length !== 0 ){
+        titleClose()
+    }
+    //선택한 input box 열기
+        disapper.className='card-title-controller-a-hide'
+        apper.className='card-title-controller-b'
+    
+}
+
+// input box 종료
+var titleClose = function(){
+    const ctrlA = document.getElementsByClassName('card-title-controller-a-hide')
+    const ctrlB = document.getElementsByClassName('card-title-controller-b')
+    if(ctrlA.length !== 0 ){
+        ctrlA[0].className='card-title-controller-a'
+        ctrlB[0].className='card-title-controller-b-hide'
+    }
+}
+
+var titleChange =function(target_h3, target_textarea, value){
+    target_h3.innerText= value
+    target_textarea._value=value
 }
