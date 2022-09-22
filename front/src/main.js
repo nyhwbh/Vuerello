40c53362f1c746907014e7f39eaa95133408eacb
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2022-09-23 08:03:48 +0900

    JsEventHandler to VueEventhandler

--- front/src/main.js
+++ front/src/main.js
@@ -2,11 +2,5 @@ import '@fortawesome/fontawesome-free/js/all.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-// get controller
-import {sendEventsClick,sendEventsEnter} from './controllers/controller'
 
 createApp(App).mount('#app')
-
-sendEventsClick()
-
-sendEventsEnter()
